7caa5558cf1e7107f8e5a4a758cf733c00e2d890
salf22 <salaird0324@gmail.com>
2019-02-08 18:36:41 -0600

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Spencer Laird on 2/8/19.
+//  Copyright © 2019 Spencer Laird. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
